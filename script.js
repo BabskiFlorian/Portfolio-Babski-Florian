28e4f222a52431fc63812949783e6e718dc955f4
BabskiFlorian <fbabski@infflux-opsoma.com>
2025-01-16 14:34:24 +0100

    comit

---
+++ script.js
@@ -0,0 +1,27 @@
+const hamburgerButton = document.querySelector(".nav-toggler")
+const navigation = document.querySelector("nav")
+const switchThemeBtn = document.querySelector('.changeTheme')
+let toggleTheme = 0;
+
+switchThemeBtn.addEventListener('click', () => {
+
+  if(toggleTheme === 0){
+
+    document.documentElement.style.setProperty('--ecriture', '#2c2c2c');
+    document.documentElement.style.setProperty('--background', '#f1f1f1');
+    toggleTheme++;
+  } else {
+    document.documentElement.style.setProperty('--ecriture', '#f1f1f1');
+    document.documentElement.style.setProperty('--background', '#2c2c2c');
+    toggleTheme--;
+  }
+
+}
+)
+
+hamburgerButton.addEventListener("click", toggleNav)
+
+function toggleNav(){
+  hamburgerButton.classList.toggle("active")
+  navigation.classList.toggle("active")
+}
